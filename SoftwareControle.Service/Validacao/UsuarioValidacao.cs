5d86d1ebab8bcf6362bb98eb8fef7f962d9bd57b
Arthur <58709410+Arthuthu@users.noreply.github.com>
2023-09-15 23:35:29 -0300

    Setup page usuarios e criar usuarios

--- SoftwareControle.Service/Validacao/UsuarioValidacao.cs
+++ SoftwareControle.Service/Validacao/UsuarioValidacao.cs
@@ -13,8 +13,8 @@ public class UsuarioValidacao : AbstractValidator<UsuarioModel>
 
         RuleFor(x => x.Senha)
             .NotEmpty().WithMessage("O campo senha não pode estar vazio")
-            .MinimumLength(8).WithMessage("O campo senha precisa ter no minimo 8 caracteres")
-            .MaximumLength(20).WithMessage("O campo senha pode ter no maximo 20 caracteres");
+            .MinimumLength(4).WithMessage("O campo senha precisa ter no minimo 4 caracteres")
+            .MaximumLength(25).WithMessage("O campo senha pode ter no maximo 20 caracteres");
 
         RuleFor(x => x.Nome)
             .NotEmpty().WithMessage("O campo nome não pode estar vazio")
