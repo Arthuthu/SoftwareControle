36c91a1c5d2e1a8e7c7d93df8a830a0b9d18a181
Arthur <58709410+Arthuthu@users.noreply.github.com>
2023-09-16 22:29:42 -0300

    Added mappers para domain entities - Usado para transferir valores de properties para objetos que estão sendo monitorados pelo entity framework

---
+++ SoftwareControle.Repositorio/DomainMapper/FerramentaModelMapper.cs
@@ -0,0 +1,24 @@
+﻿using SoftwareControle.Models;
+
+namespace SoftwareControle.Repositorio.DomainMapper;
+
+public static class FerramentaModelMapper
+{
+    public static FerramentaModel MapFerramentaModel(this FerramentaModel ferramentaModel)
+    {
+        return new FerramentaModel()
+        {
+            Id = ferramentaModel.Id,
+            Nome = ferramentaModel.Nome,
+            Descricao = ferramentaModel.Descricao,
+            Disponivel = ferramentaModel.Disponivel,
+            Imagem = ferramentaModel.Imagem,
+            DataCriacao = ferramentaModel.DataCriacao,
+            DataAtualizacao = ferramentaModel.DataAtualizacao,
+            UsuarioId = ferramentaModel.UsuarioId,
+            Ordem = ferramentaModel.Ordem,
+            Usuario = ferramentaModel.Usuario
+        };
+
+    }
+}
---
+++ SoftwareControle.Repositorio/DomainMapper/UsuarioModelMapper.cs
@@ -0,0 +1,23 @@
+﻿using SoftwareControle.Models;
+
+namespace SoftwareControle.Repositorio.DomainMapper;
+
+public static class UsuarioModelMapper
+{
+    public static UsuarioModel MapUsuarioModel(this UsuarioModel usuarioModel)
+    {
+        return new UsuarioModel() 
+        {
+            Id = usuarioModel.Id,
+            Usuario = usuarioModel.Usuario,
+            Senha = usuarioModel.Senha,
+            Nome = usuarioModel.Nome,
+            Cargo = usuarioModel.Cargo,
+            DataCriacao = usuarioModel.DataCriacao,
+            DataAtualizacao = usuarioModel.DataAtualizacao,
+            Ordem = usuarioModel.Ordem,
+            Ferramenta = usuarioModel.Ferramenta
+        };
+
+    }
+}
